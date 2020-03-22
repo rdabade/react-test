17594373840fc788a51b8b8bf0ed0e026abec011
Rohan <rohannatureps@gmail.com>
2020-03-22 17:22:11 +0530

    INITIAL COMMIT

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import User from './components/User';
+
+function App() {
+  return (
+    <div className="App">
+     Helo <User />
+    </div>
+  );
+}
+
+export default App;
